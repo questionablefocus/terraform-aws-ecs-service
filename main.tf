c66482316f1ccdd218d54a4eb3be6c1f8579d189
Marcus Mo <marcus.mo@questionablefocus.com>
2025-06-03 11:05:34 +0100

    Add support for additional ingress rules for the service

--- main.tf
+++ main.tf
@@ -17,6 +17,17 @@ resource "aws_security_group" "main" {
   description = "Security group for ECS service"
   vpc_id      = var.vpc_id
 
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = try(ingress.value.description, null)
+    }
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
--- variables.tf
+++ variables.tf
@@ -55,3 +55,15 @@ variable "task_role_arn" {
   description = "ARN of the task role"
   type        = string
 }
+
+variable "ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = optional(string)
+  }))
+  default = []
+}
