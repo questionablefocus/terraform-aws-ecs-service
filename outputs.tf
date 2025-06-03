48d30dcfda3e46329ce3a2e6e626d2a9306f98b6
Marcus Mo <marcus.mo@questionablefocus.com>
2025-06-03 11:58:48 +0100

    Expose EFS related outputs

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,28 @@ output "task_definition_arn" {
   value       = aws_ecs_task_definition.main.arn
 }
 
+output "efs_file_system_id" {
+  description = "ID of the EFS file system"
+  value       = var.efs_root_directory != null ? aws_efs_file_system.main[0].id : null
+}
+
+output "efs_file_system_arn" {
+  description = "ARN of the EFS file system"
+  value       = var.efs_root_directory != null ? aws_efs_file_system.main[0].arn : null
+}
+
+output "efs_access_point_id" {
+  description = "ID of the EFS access point"
+  value       = var.efs_root_directory != null ? aws_efs_access_point.main[0].id : null
+}
+
+output "efs_access_point_arn" {
+  description = "ARN of the EFS access point"
+  value       = var.efs_root_directory != null ? aws_efs_access_point.main[0].arn : null
+}
+
+output "efs_security_group_id" {
+  description = "ID of the EFS security group"
+  value       = var.efs_root_directory != null ? aws_security_group.efs[0].id : null
+}
+
