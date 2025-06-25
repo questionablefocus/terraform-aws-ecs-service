897f8f192e8aa6ccb5329b0cfa66b5ce29de4f99
Marcus Mo <marcus.mo@questionablefocus.com>
2025-06-25 14:29:36 +0100

    Use cluster name instead of cluster id

--- autoscaling.tf
+++ autoscaling.tf
@@ -2,7 +2,7 @@ resource "aws_appautoscaling_target" "main" {
   count              = var.enable_autoscaling ? 1 : 0
   max_capacity       = var.max_capacity
   min_capacity       = var.min_capacity
-  resource_id        = "service/${var.cluster_id}/${aws_ecs_service.main.name}"
+  resource_id        = "service/${var.cluster_name}/${aws_ecs_service.main.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
--- variables.tf
+++ variables.tf
@@ -23,6 +23,11 @@ variable "cluster_id" {
   type        = string
 }
 
+variable "cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+}
+
 variable "vpc_id" {
   description = "ID of the VPC"
   type        = string
